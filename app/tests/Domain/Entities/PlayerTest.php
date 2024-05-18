b10e39079573b5fe9cdb7ff5286701c8738a6333
Carlos Batista <cbatista8a@gmail.com>
2024-05-18 15:03:59 +0200

    [test] - add Player test cases

---
+++ app/tests/Domain/Entities/PlayerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cbatista8a\Tris\Domain\Entities;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+
+class PlayerTest extends TestCase
+{
+    private Player $player;
+
+    public function setUp(): void
+    {
+        $this->player = new Player(1, 'Carlos');
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testIncreaseScore(): void
+    {
+        $number = random_int(1, 5);
+        for ($i = 0; $i < $number; $i++){
+            $this->player->increaseScore();
+        }
+        $this->assertEquals($number, $this->player->getScore());
+    }
+
+    public function testGetSymbol(): void
+    {
+        $this->assertEquals('C', $this->player->getSymbol());
+    }
+}
