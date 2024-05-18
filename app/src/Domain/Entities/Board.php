def9a70d92712d41d6adad11d25c18c922063c12
Carlos Batista <cbatista8a@gmail.com>
2024-05-18 13:18:36 +0200

    [feat] - models a Board entity

---
+++ app/src/Domain/Entities/Board.php
@@ -0,0 +1,32 @@
+<?php
+
+class Board
+{
+    private int $id;
+    private array $matrix = [1,2,3,4,5,6,7,8,9];
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getMatrix(): array
+    {
+        return $this->matrix;
+    }
+
+
+    /**
+     * @param int $position
+     * @param string $symbol
+     * @return void
+     */
+    public function updateMatrix(int $position, string $symbol): void
+    {
+        $this->matrix[$position] = $symbol;
+    }
+}
