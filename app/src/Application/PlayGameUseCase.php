83a494b67e8162f1985d5fd6ceb610c8ae512b5c
Carlos Batista <cbatista8a@gmail.com>
2024-05-18 17:35:27 +0200

    [feat] - add PlayGame application service

---
+++ app/src/Application/PlayGameUseCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cbatista8a\Tris\Application;
+
+use Cbatista8a\Tris\Domain\Entities\Game;
+use Cbatista8a\Tris\Domain\Entities\Player;
+use Cbatista8a\Tris\Domain\Interfaces\RepositoryInterface;
+
+class PlayGameUseCase
+{
+
+    private RepositoryInterface $repository;
+
+    public function __construct(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function __invoke(int $game_id, Player $player, int $position): array
+    {
+        /** @var Game $game */
+        $game = $this->repository->find(Game::class, $game_id);
+        $game->play($player, $position -1); // -1 because the board is 0 indexed
+        $this->repository->save($game);
+
+        $checkWinner = $game->checkWinner();
+        return [
+            'board' => $game->getBoard()->getMatrix(),
+            'has_winner' => $checkWinner !== null,
+            'winner' => $checkWinner,
+        ];
+    }
+}
