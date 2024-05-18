9bd2ddf56e533856d2ba8f3023f540745558ef55
Carlos Batista <cbatista8a@gmail.com>
2024-05-18 18:39:00 +0200

    [feat] - use index.php as router to simplify

--- app/index.php
+++ app/index.php
@@ -1,2 +1,31 @@
 <?php
-phpinfo();
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+use Cbatista8a\Tris\Infrastructure\Controllers\NewGameController;
+use Cbatista8a\Tris\Infrastructure\Controllers\PlayGameController;
+use Cbatista8a\Tris\Infrastructure\InMemoryRepository;
+
+session_start();
+$repository = new InMemoryRepository();
+$response = [];
+
+$action = $_GET['action'] ?? null;
+$controller = null;
+switch ($action) {
+    case 'new_game':
+        $controller = new NewGameController($repository);
+        break;
+    case 'play':
+        $controller = new PlayGameController($repository);
+        break;
+}
+
+if ($controller) {
+    $response = $controller();
+}
+
+$response['help'] = 'Use the following parameters: action=new_game to start a new game | action=play&player_id=1&game_id=1&position=1 to play a game.';
+echo json_encode($response);
+exit();
